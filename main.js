bce942d527f42d737c94cdd4b4384dc91afe0961
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-25 23:51:51 -0300

    Create style app

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 const path = require("path");
-const { app, BrowserWindow } = requere("electron");
+const { app, BrowserWindow } = require("electron");
+
+const isDev = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
 
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
+
+//App is ready
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+// Menu template
+const menu = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+        accelerator: "CmdOrCtrl+W",
+      },
+    ],
+  },
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
