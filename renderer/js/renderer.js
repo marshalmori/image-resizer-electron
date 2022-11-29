2ae38434bdc5f6bfda4e4fa2fe2e251ca034c034
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-28 21:37:29 -0300

    Show output path

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -23,6 +23,7 @@ function loadImage(e) {
 
   form.style.display = "block";
   filename.innerText = file.name;
+  outputPath.innerText = path.join(os.homedir(), "Imagens");
 }
 
 // Make sure file is Image
